7aaad1e49c6d597b594be9bfc379b859cb9e58aa
mr <cds>
2022-06-15 12:21:14 +0200

    NOTE MUSICALI + SOUND 2

--- XilophoneSoundMaker/ViewController.swift
+++ XilophoneSoundMaker/ViewController.swift
@@ -11,24 +11,112 @@ import AVFoundation
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var buttonSoundA: UIButton!
+    @IBOutlet weak var buttonSoundB: UIButton!
+    @IBOutlet weak var buttonSoundC: UIButton!
+    @IBOutlet weak var buttonSoundD: UIButton!
+    @IBOutlet weak var buttonSoundE: UIButton!
+    
+    @IBOutlet var buttonSoundF: UIButton!
+    @IBOutlet var buttonSoundG: UIButton!
+    
+   
     var player : AVAudioPlayer!
     
     @IBAction func keyPressed(_ sender: UIButton) {
         print("Call here")
-        playSound()
+        
+        /*
+        switch sender.tag{
+            
+        case 1:
+            playSound(nameOfSound: "A")
+            break
+        case 2:
+            playSound(nameOfSound: "B")
+            break
+        
+        default: break
+        }
+        */
+        
+        if(sender == buttonSoundA){
+            playSound(nameOfSound: "DO")
+        }
+        else if(sender == buttonSoundB){
+
+            playSound(nameOfSound: "RE")
+
+        }
+        else if(sender == buttonSoundC){
+            playSound(nameOfSound: "MI")
+
+        }
+        else if(sender == buttonSoundD){
+            playSound(nameOfSound: "FA")
+
+        }
+        else if(sender == buttonSoundE ) {
+            playSound(nameOfSound: "SOL")
+
+        }
+        else if(sender == buttonSoundF){
+            playSound(nameOfSound: "LA")
+
+        }
+        else if(sender == buttonSoundG){
+            playSound(nameOfSound: "SI")
+
+        }
+        
+        
     }
+    
+    
+    func soundAMelody(){
+        
+        
+        // do re fa mi re sol sol
+        // sol la mi fa re fa mi re do
+        
+        do {
+            playSound(nameOfSound: "SI")
+            sleep(1)
+            playSound(nameOfSound: "SI")
+            sleep(1)
+            playSound(nameOfSound: "DO")
+            sleep(1)
+            playSound(nameOfSound: "RE")
+            sleep(1)
+            playSound(nameOfSound: "RE")
+            sleep(1)
+            playSound(nameOfSound: "DO")
+            sleep(1)
+            playSound(nameOfSound: "SI")
+            sleep(1)
+            playSound(nameOfSound: "LA")
+
+       
+        }
+        
+    }
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        sleep(4)
+        soundAMelody()
+        
     }
 
 
-    func playSound() {
+    func playSound(nameOfSound : String) {
         
         print("#####")
         
-        guard let url = Bundle.main.url(forResource: "p", withExtension: "mp3") else {
+        guard let url = Bundle.main.url(forResource: "mp3/" + nameOfSound, withExtension: "mp3") else {
             print("RETURNING ")
             return
         }
